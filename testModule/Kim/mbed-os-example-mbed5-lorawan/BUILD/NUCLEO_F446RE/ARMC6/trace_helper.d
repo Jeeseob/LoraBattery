78af68541bf420eb5f29e54a5aafffc7bf49cc4e
Jeeseob <jeeseob5761@gmail.com>
2022-01-12 14:04:42 +0900

    test

--- testModule/Kim/mbed-os-example-mbed5-lorawan/BUILD/NUCLEO_F446RE/ARMC6/trace_helper.d
+++
@@ -1,4 +0,0 @@
-BUILD/NUCLEO_F446RE/ARMC6/trace_helper.o: trace_helper.cpp \
-  BUILD/NUCLEO_F446RE/ARMC6/mbed_config.h \
-  mbed-os/features/frameworks/mbed-trace/mbed-trace/mbed_trace.h \
-  mbed-os/features/frameworks/nanostack-libservice/mbed-client-libservice/ns_types.h
